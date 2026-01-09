f24b87e5bd1195533c72cebe7b371f38de6879c5
skadoosh-MC <152390552+skadoosh-MC@users.noreply.github.com>
2026-01-09 17:32:53 +0000

    Test BAO

---
+++ tests/test_physics.py
@@ -0,0 +1,20 @@
+import numpy as np
+from main import emulate_pknl
+
+def test_bao_rewiggling(linear_power):
+    c = linear_power
+
+    k, pknl, pknl_nw = emulate_pknl(
+        c["k"],
+        c["pk"],
+        c["h"],
+        c["Omegam"],
+        c["Omegab"],
+        c["ns"],
+        c["sigma8"],
+        model="spine"
+    )
+
+    # Wiggles must change the spectrum
+    diff = np.std(pknl - pknl_nw)
+    assert diff > 0
