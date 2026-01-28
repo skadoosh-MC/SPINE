2581a3f1f4557e77ae3ec5ac644debbd945c4c2b
skadoosh-MC <152390552+skadoosh-MC@users.noreply.github.com>
2026-01-28 21:25:23 +0000

    Update class_spinex.py

--- spine_pk/class_spinex.py
+++ spine_pk/class_spinex.py
@@ -21,7 +21,7 @@ from sklearn.metrics import mean_squared_error
 from sklearn.metrics import r2_score
 from sklearn.metrics import mean_absolute_percentage_error
 import math
-from Emulators.growth import GrowthCalculator
+from spine_pk.growth import GrowthCalculator
 from scipy.integrate import simpson
 from functools import lru_cache
 
